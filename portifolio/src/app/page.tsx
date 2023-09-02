a35167dcaa94521f5a423ecbb570736106abc6f4
Matheus Hora <matheus11hora@gmail.com>
2023-09-02 16:04:52 -0300

    coemçando o sobre

--- portifolio/src/app/page.tsx
+++ portifolio/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import styles from "../styles/page.module.css";
 import Initial from "@/components/initia/initial";
+import About from "@/components/about/about";
 
 export default function Home() {
   return (
     <main>
       <Initial />
+      <About />
     </main>
   );
 }
---
+++ portifolio/src/components/about/about.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import Image from "next/image";
+import Style from "./about.module.css";
+import Photo from "../../../public/images/designer.svg";
+
+const About = () => {
+  return (
+    <section className={Style.about}>
+      <h2>Sobre</h2>
+      <div className={Style.content}>
+        <div className={Style.image}>
+          <Image src={Photo} alt={"Icon designer"} />
+        </div>
+        <div className={Style.text}>
+          <h3>
+            <span>Transformando</span> linhas de código em experiências únicas.
+          </h3>
+          <p>
+            Ei, tudo certo? Sou um desenvolvedor Flutter apaixonado por inovação
+            e tecnologia. Com maestria em Dart e Flutter, estou pronto para
+            liderar desafios e colaborar com equipes talentosas. Minha base
+            sólida em programação, design de apps e experiência do usuário me
+            destaca como um Engenheiro de Software de alto nível.
+          </p>
+          <br />
+          <p>
+            Busco oportunidades incríveis para crescer e me aprimorar em um
+            ambiente colaborativo. Se você procura um profissional que abraça
+            desafios e é movido pela paixão pela tecnologia, sua busca termina
+            aqui!
+          </p>
+        </div>
+      </div>
+
+      <div></div>
+    </section>
+  );
+};
+export default About;

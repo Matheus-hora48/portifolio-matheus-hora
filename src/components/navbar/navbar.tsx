787cfaf0610ad6240b941b4bcf8c3796c8a3a3fd
Anderson Junior <acfaj2010@gmail.com>
2023-09-14 16:20:42 -0300

    feat: Hamburguer menu added

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,7 +1,15 @@
+"use client";
 import Link from "next/link";
 import style from "./navbar.module.css";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [burguerOpen, setBurguerOpen] = useState(false);
+
+  const handleBurguer = () => {
+    setBurguerOpen(!burguerOpen);
+  };
+
   return (
     <div className={style.navbar}>
       <div className={style.title}>
@@ -12,6 +20,24 @@ const Navbar = () => {
         <Link href={""}>Projetos</Link>
         <button className={style.button}>Contato</button>
       </div>
+      <button className={style.burguerButton} onClick={handleBurguer}>
+        <span className={style.burguerIcon}></span>
+        <div
+          className={`${style.burguerContentClosed} ${
+            burguerOpen ? style.burguerContent : ""
+          }`}
+        >
+          <Link href={""} className={style.buttonBurguer}>
+            Sobre mim
+          </Link>
+          <Link href={""} className={style.buttonBurguer}>
+            Projetos
+          </Link>
+          <Link href={""} className={style.buttonBurguer}>
+            Contato
+          </Link>
+        </div>
+      </button>
     </div>
   );
 };
